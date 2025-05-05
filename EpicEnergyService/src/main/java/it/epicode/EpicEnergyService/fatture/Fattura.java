d3c1c02e80136f7fb321345db2e7f5b38699ffa8
Zephyrior <mark.temblique@gmail.com>
2025-05-05 13:38:46 +0200

    creazione classe fattura & piccola modifica classe cliente

---
+++ EpicEnergyService/src/main/java/it/epicode/EpicEnergyService/fatture/Fattura.java
@@ -0,0 +1,36 @@
+package it.epicode.EpicEnergyService.fatture;
+
+import it.epicode.EpicEnergyService.clienti.Cliente;
+import it.epicode.EpicEnergyService.enums.Stato;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "fatture")
+public class Fattura {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long numero;
+
+    @Column(nullable = false)
+    private LocalDate data;
+
+    @Column(nullable = false)
+    private Long importo;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Stato stato;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    private Cliente cliente;
+
+}
