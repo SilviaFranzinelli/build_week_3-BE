41d15ffa65b26ca04516513d24beef14f804ddee
Fabio-Bertolino <fabiobertix97@gmail.com>
2025-05-05 12:42:34 +0200

    Creazione Entity Cliente

---
+++ EpicEnergyService/src/main/java/it/epicode/EpicEnergyService/clienti/Cliente.java
@@ -0,0 +1,50 @@
+package it.epicode.EpicEnergyService.clienti;
+
+import it.epicode.EpicEnergyService.enums.TipoCliente;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "clienti")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String ragioneSociale;
+    @Column(nullable = false, length = 11)
+    private String partitaIva;
+    @Column(nullable = false, length = 100)
+    private String email;
+    @Column(nullable = false)
+    private LocalDate dataInserimento;
+    @Column(nullable = false)
+    private LocalDate dataUltimoContatto;
+    @Column(nullable = false)
+    private Long fatturatoAnnuale;
+    @Column(nullable = false, length = 100)
+    private String pec;
+    @Column(nullable = false, length = 100)
+    private String telefono;
+    @Column(nullable = false, length = 100)
+    private String emailContatto;
+    @Column(nullable = false, length = 100)
+    private String nomeContatto;
+    @Column(nullable = false, length = 100)
+    private String cognomeContatto;
+    @Column(nullable = false, length = 100)
+    private String telefonoContatto;
+
+    private String logoAziendale;
+
+    private TipoCliente tipoCliente;
+}
